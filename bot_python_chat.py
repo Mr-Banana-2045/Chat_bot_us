e867993fe152d7b2ca71f766a6c55235ecb5780c
Moz-Banana <109140672+Mr-Banana-2045@users.noreply.github.com>
2023-05-30 23:34:07 +0330

    Update bot_python_chat.py

--- bot_python_chat.py
+++ bot_python_chat.py
@@ -1,5 +1,4 @@
-import time
-import datetime
+
 
 def find(txt, text):
 	msg = text.split()
@@ -7,85 +6,14 @@ def find(txt, text):
 	if txt in msg:
 		return "\033[94mBot:\033[92m hello my friend"
 	elif koja in msg:
-		return "\033[94mBot:\033[92m in my pocket"
-	elif kojast in msg:
-		return "\033[94mBot:\033[92m in my pocket"
-	elif chi in msg:
-		return "\033[94mBot:\033[92m I don't know, I'm not in the news"
-	elif bash in msg:
-		return "\033[94mBot:\033[92m Yes, in short"
-	elif ki in msg:
-		return "\033[94mBot:\033[92m my aunt"
-	elif chet in msg:
-		return "\033[94mBot:\033[92m not your business"
-	elif chetori in msg:
-		return "\033[94mBot:\033[92m not your business"
-	elif migam in msg:
-		return "\033[94mBot:\033[92m to this me"
-	elif goftam in msg:
-		return "\033[94mBot:\033[92m to this me"
-	elif inja in msg:
-		return "\033[94mBot:\033[92m my aunt's pocket"
-	elif bay in msg:
-		return "\033[94mBot:\033[92m Go away so I don't see you again"
-	elif emroz in msg:
-		return "\033[94mBot:\033[92m I don't know brother"
-	elif diroz in msg:
-		return "\033[94mBot:\033[92m I don't know brother"
-	elif farda in msg:
-		return "\033[94mBot:\033[92m I don't know brother"
-	elif emsal in msg:
-		return "\033[94mBot:\033[92m it's not my business"
-	elif parisal in msg:
-		return "\033[94mBot:\033[92m it's not my business"
-	elif saldige in msg:
-		return "\033[94mBot:\033[92m it's not my business"
-	elif time in msg:
-		now = datetime.datetime.now()
-		times = now.strftime("%H:%M:%S")
-		return "\033[94mBot:\033[92m "+times
-	elif sen in msg:
-		return "\033[94mBot:\033[92m not your business"
-	elif chera in msg:
-		return "\033[94mBot:\033[92m because because"
-	elif to in msg:
-		return "\033[94mBot:\033[92m not your business"
-	elif man in msg:
-		return "\033[94mBot:\033[92m okey"
-	elif maan in msg:
-		return "\033[94mBot:\033[92m okey"
-	elif date in msg:
-		nom = datetime.datetime.now()
-		dates = nom.strftime("%d/%m/%Y")
-		return "\033[94mBot:\033[92m "+dates
+		return "\033[94mBot:\033[92m bye my friend"
+	
 	else:
 		return "\033[91merror"
 		
 txt = "hello"
-koja = "where"
-kojast = "where is it"
-chi = "what"
-bash = "ok"
-ki = "when"
-chet = "how"
-chetori = "how arw you"
-migam = "i say"
-inja = "here"
-bay = "bye"
-goftam = "i said"
-emroz = "today"
-diroz = "yesterday"
-farda = "tomorrow"
-emsal = "year"
-parisal = "last year"
-saldige = "next year"
-time = "time"
-sen = "age"
-chera = "why"
-to = "you"
-man = "me"
-maan = "my"
-date = "date"
+koja = "bye"
+
 
 while True:
 	text = input("\033[93myou > \033[97m")
